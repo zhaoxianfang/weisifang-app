c8927efc9ddcc0dfa801ad6fba705176fe5beafe
zhaoxianfang <1748331509@qq.com>
2022-08-13 18:08:51 +0800

    fix

--- js_sdk/weisifang/helper.js
+++ js_sdk/weisifang/helper.js
@@ -7,6 +7,88 @@ isIos = (plus.os.name === 'iOS')
 const helper = {
     debounceTime: null, // 防抖计时器
     debounceOldArgs: [], // 防抖老参数
+    init() {
+    // #ifdef APP-PLUS
+    // 判断通知权限
+        this.judgeIosPermissionPush()
+        // app 检测版本更新
+        this.checkAppUpgrade(0)
+        this.clickToBack()
+        this.checkWhiteList()
+
+        // var main = plus.android.runtimeMainActivity(); //获取activity  
+        // var Intent = plus.android.importClass('android.content.Intent');  
+        // var Settings = plus.android.importClass('android.provider.Settings');  
+        // var intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);//可设置表中所有Action字段  
+        // main.startActivity(intent);
+
+        this.requestPermission()
+    // #endif
+    },
+    // 返回键如何进入后台
+    clickToBack() {
+        var main = plus.android.runtimeMainActivity()
+        main.moveTaskToBack(false)
+    },
+    // 判断是否在白名单中
+    checkWhiteList() {
+    // 白名单  
+        var main = plus.android.runtimeMainActivity()
+        var packName = main.getPackageName()
+        var Context = plus.android.importClass('android.content.Context')
+        var PowerManager = plus.android.importClass('android.os.PowerManager')
+        // 获取电源类  
+        var pm = main.getSystemService(Context.POWER_SERVICE)
+        console.log('是否在白名单：', pm.isIgnoringBatteryOptimizations(packName)) //是否白名单  
+        let whiteList = pm.isIgnoringBatteryOptimizations(packName)
+        try {
+            var Uri = plus.android.importClass('android.net.Uri')
+            var Settings = plus.android.importClass('android.provider.Settings')
+            var packageURI = Uri.parse('package:' + packName)
+            var intents = plus.android.newObject('android.content.Intent', Settings
+                .ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS, packageURI) // 电池  
+            main.startActivity(intents)
+        } catch {
+            console.log('调起失败')
+        }
+    },
+    // 判断权限
+    requestPermission() {
+        plus.android.requestPermissions(
+            ['android.permission.ACCESS_FINE_LOCATION', 'android.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS',
+                'android.permission.WAKE_LOCK',
+                'android.permission.WRITE_EXTERNAL_STORAGE', 'android.permission.RECEIVE_BOOT_COMPLETED'
+            ],
+            function(resultObj) {
+                for (var i = 0; i < resultObj.granted.length; i++) {
+                    var grantedPermission = resultObj.granted[i]
+                    console.log('已获取的权限：' + grantedPermission)
+                }
+                for (var i = 0; i < resultObj.deniedPresent.length; i++) {
+                    var deniedPresentPermission = resultObj.deniedPresent[i]
+                    console.log('拒绝本次申请的权限：' + deniedPresentPermission)
+                }
+                for (var i = 0; i < resultObj.deniedAlways.length; i++) {
+                    var deniedAlwaysPermission = resultObj.deniedAlways[i]
+                    console.log('永久拒绝申请的权限：' + deniedAlwaysPermission)
+                }
+                // 若所需权限被永久拒绝,则打开APP设置界面,可以在APP设置界面打开相应权限  
+                if (resultObj.deniedAlways.length > 0) {
+                    var Intent = plus.android.importClass('android.content.Intent')
+                    var Settings = plus.android.importClass('android.provider.Settings')
+                    var Uri = plus.android.importClass('android.net.Uri')
+                    var mainActivity = plus.android.runtimeMainActivity()
+                    var intent = new Intent()
+                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    var uri = Uri.fromParts('package', mainActivity.getPackageName(), null)
+                    intent.setData(uri)
+                    mainActivity.startActivity(intent)
+                }
+            },
+            function(error) {
+                console.log('申请权限错误：' + error.code + ' = ' + error.message)
+            })
+    },
     // 检测app 版本更新
     //获取线上APP版本信息  参数type 0自动检查  1手动检查（手动检查时，之前取消更新的版本也会提示出来）
     checkAppUpgrade: (type = 0) => {
