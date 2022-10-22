cf9f81d00fd86881fb4a2ab5c704d0c77b142b20
zhaoxianfang <1748331509@qq.com>
2022-10-22 18:31:54 +0800

    fix

--- pages/tabbar/my/my.vue
+++ pages/tabbar/my/my.vue
@@ -6,11 +6,12 @@
 				<!--个人中心页为主页面，不应有返回键-->
 				<!-- #ifndef MP -->
 				<view class="tui-header-icon">
-					<view class="tui-icon-box tui-icon-message" @tap="href(7)">
-						<tui-icon name="message" :color="opacity > 0.8 ? '#333' : '#fff'" :size="26"></tui-icon>
+          <view class="tui-icon-box tui-icon-tools" @tap="scanCode"><tui-icon name="sweep" :color="opacity > 0.8 ? '#333' : '#fff'" :size="26"></tui-icon></view>
+					<view class="tui-icon-box tui-icon-tools" @tap="href(7)">
+						<tui-icon name="message" :color="opacity > 0.8 ? '#333' : '#fff'" :size="28"></tui-icon>
 						<view class="tui-badge" :class="[opacity > 0.8 ? 'tui-badge-red' : 'tui-badge-white']">1</view>
 					</view>
-					<view class="tui-icon-box tui-icon-setup" @tap="href(2)"><tui-icon name="setup" :color="opacity > 0.8 ? '#333' : '#fff'" :size="26"></tui-icon></view>
+					<view class="tui-icon-box tui-icon-tools" @tap="href(2)"><tui-icon name="setup" :color="opacity > 0.8 ? '#333' : '#fff'" :size="28"></tui-icon></view>
 				</view>
 				<!-- #endif -->
 			</view>
@@ -32,11 +33,12 @@
 				<!-- #endif -->
 				<!-- #ifdef MP -->
 				<view class="tui-set-box">
-					<view class="tui-icon-box tui-icon-message" @tap="href(7)">
-						<tui-icon name="message" color="#fff" :size="26"></tui-icon>
+          <view class="tui-icon-box tui-icon-tools" @tap="scanCode"><tui-icon name="sweep" :color="opacity > 0.8 ? '#333' : '#fff'" :size="26"></tui-icon></view>
+					<view class="tui-icon-box tui-icon-tools" @tap="href(7)">
+						<tui-icon name="message" color="#fff" :size="28"></tui-icon>
 						<view class="tui-badge tui-badge-white">1</view>
 					</view>
-					<view class="tui-icon-box tui-icon-setup" @tap="href(2)"><tui-icon name="setup" color="#fff" :size="26"></tui-icon></view>
+					<view class="tui-icon-box tui-icon-tools" @tap="href(2)"><tui-icon name="setup" color="#fff" :size="28"></tui-icon></view>
 				</view>
 				<!-- #endif -->
 			</view>
@@ -59,129 +61,45 @@
 				</view>
 			</view>
 		</view>
+
 		<view class="tui-content-box">
-			<view class="tui-box tui-order-box">
-				<tui-list-cell :arrow="true" padding="0" :lineLeft="false" @click="href(4)">
-					<view class="tui-cell-header">
-						<view class="tui-cell-title">我的订单</view>
-						<view class="tui-cell-sub">查看全部订单</view>
+			<tui-list-view title="">
+				<tui-list-cell @click="detail" :arrow="true">
+					<view class="tui-item-box">
+						<tui-icon name="wealth-fill" :size="24" color="#ff7900"></tui-icon>
+						<text class="tui-list-cell_name">我的钱包</text>
 					</view>
 				</tui-list-cell>
-				<view class="tui-order-list">
-					<view class="tui-order-item" @tap="href(4)">
-						<view class="tui-icon-box">
-							<image src="/static/images/mall/my/icon_daifukuan_3x.png" class="tui-order-icon"></image>
-							<view class="tui-badge tui-badge-red">1</view>
-						</view>
-						<view class="tui-order-text">待付款</view>
-					</view>
-					<view class="tui-order-item" @tap="href(4)">
-						<view class="tui-icon-box">
-							<image src="/static/images/mall/my/icon_daifahuo_3x.png" class="tui-order-icon"></image>
-							<view class="tui-badge tui-badge-red">1</view>
-						</view>
-						<view class="tui-order-text">待发货</view>
-					</view>
-					<view class="tui-order-item" @tap="href(4)">
-						<view class="tui-icon-box"><image src="/static/images/mall/my/icon_daishouhuo_3x.png" class="tui-order-icon"></image></view>
-						<view class="tui-order-text">待收货</view>
-					</view>
-					<view class="tui-order-item" @tap="href(4)">
-						<view class="tui-icon-box">
-							<image src="/static/images/mall/my/icon_pingjia_3x.png" class="tui-order-icon"></image>
-							<view class="tui-badge tui-badge-red" v-if="false">12</view>
-						</view>
-						<view class="tui-order-text">评价</view>
-					</view>
-					<view class="tui-order-item" @tap="href(10)">
-						<view class="tui-icon-box">
-							<image src="/static/images/mall/my/icon_tuikuan_3x.png" class="tui-order-icon"></image>
-							<view class="tui-badge tui-badge-red">2</view>
-						</view>
-						<view class="tui-order-text">退款/售后</view>
-					</view>
-				</view>
-			</view>
-
-			<view class="tui-box tui-assets-box">
-				<tui-list-cell padding="0" unlined :hover="false">
-					<view class="tui-cell-header"><view class="tui-cell-title">我的资产</view></view>
-				</tui-list-cell>
-				<view class="tui-order-list tui-assets-list">
-					<view class="tui-order-item" @tap="href(8)">
-						<view class="tui-assets-num">
-							<text>3</text>
-							<view class="tui-badge-dot"></view>
-						</view>
-						<view class="tui-assets-text">优惠券</view>
-					</view>
-					<view class="tui-order-item">
-						<view class="tui-assets-num">
-							<text>7</text>
-							<view class="tui-badge-dot"></view>
-						</view>
-						<view class="tui-assets-text">积分</view>
-					</view>
-					<view class="tui-order-item">
-						<view class="tui-assets-num"><text>0</text></view>
-						<view class="tui-assets-text">红包</view>
-					</view>
-					<view class="tui-order-item">
-						<view class="tui-assets-num"><text>1</text></view>
-						<view class="tui-assets-text">礼品卡</view>
-					</view>
-					<view class="tui-order-item">
-						<view class="tui-assets-num"><text>20</text></view>
-						<view class="tui-assets-text">津贴</view>
-					</view>
-				</view>
-			</view>
-
-			<view class="tui-box tui-tool-box">
-				<tui-list-cell :arrow="true" padding="0" :lineLeft="false">
-					<view class="tui-cell-header">
-						<view class="tui-cell-title">常用工具</view>
-						<view class="tui-cell-sub">查看更多</view>
+				<tui-list-cell @click="detail" :arrow="true">
+					<view class="tui-item-box">
+						<tui-icon name="service-fill" :size="24" color="#5677fc"></tui-icon>
+						<view class="tui-list-cell_name">服务窗</view>
 					</view>
 				</tui-list-cell>
-				<view class="tui-order-list tui-flex-wrap">
-					<view class="tui-tool-item">
-						<view class="tui-icon-box">
-							<image src="/static/images/mall/my/icon_gift_3x.png" class="tui-tool-icon"></image>
-							<image src="/static/images/mall/my/icon_tab_3x.png" class="tui-badge-icon"></image>
+				<tui-list-cell @click="detail" :arrow="true">
+					<view class="tui-item-box">
+						<tui-icon name="explore-fill" :size="24" color="#19be6b"></tui-icon>
+						<view class="tui-list-cell_name">发现</view>
+						<view class="tui-ml-auto">
+							<tui-tag padding="10rpx 12rpx" margin="0 30rpx 0 0" size="24rpx" type="light-green" shape="circle">探索发现</tui-tag>
 						</view>
-						<view class="tui-tool-text">免费领好礼</view>
 					</view>
-					<view class="tui-tool-item">
-						<view class="tui-icon-box"><image src="/static/images/mall/my/icon_purse_3x.png" class="tui-tool-icon"></image></view>
-						<view class="tui-tool-text">钱包</view>
+				</tui-list-cell>
+				<tui-list-cell @click="detail" :arrow="true">
+					<view class="tui-item-box">
+						<tui-icon name="shop-fill" :size="23" color="#ed3f14"></tui-icon>
+						<view class="tui-list-cell_name">我的店铺</view>
+						<view class="tui-right">进入店铺</view>
 					</view>
-					<view class="tui-tool-item" @tap="href(6)">
-						<view class="tui-icon-box"><image src="/static/images/mall/my/icon_ticket_3x.png" class="tui-tool-icon"></image></view>
-						<view class="tui-tool-text">领券中心</view>
+				</tui-list-cell>
+				<tui-list-cell @click="detail" :arrow="true" last="true">
+					<view class="tui-item-box">
+						<image src="/static/images/my/thorui.png" class="tui-logo" mode="widthFix"></image>
+						<text class="tui-list-cell_name">关于</text>
+						<view class="tui-right">Thor UI</view>
 					</view>
-					<view class="tui-tool-item">
-						<view class="tui-icon-box"><image src="/static/images/mall/my/icon_kefu_3x.png" class="tui-tool-icon"></image></view>
-						<view class="tui-tool-text">客服服务</view>
-					</view>
-					<view class="tui-tool-item">
-						<view class="tui-icon-box"><image src="/static/images/mall/my/icon_fanxian_3x.png" class="tui-tool-icon"></image></view>
-						<view class="tui-tool-text">每日返现</view>
-					</view>
-					<view class="tui-tool-item" @tap="href(9)">
-						<view class="tui-icon-box"><image src="/static/images/mall/my/icon_tuan_3x.png" class="tui-tool-icon"></image></view>
-						<view class="tui-tool-text">我的拼团</view>
-					</view>
-					<view class="tui-tool-item">
-						<view class="tui-icon-box"><image src="/static/images/mall/my/icon_zhihuan_3x.png" class="tui-tool-icon"></image></view>
-						<view class="tui-tool-text">闲置换钱</view>
-					</view>
-					<view class="tui-tool-item">
-						<view class="tui-icon-box"><image src="/static/images/mall/my/icon_kaipiao_3x.png" class="tui-tool-icon"></image></view>
-						<view class="tui-tool-text">开发票</view>
-					</view>
-				</view>
-			</view>
+				</tui-list-cell>
+			</tui-list-view>
 
 			<!--为你推荐-->
 			<tui-divider :size="28" :bold="true" color="#333" width="50%">为你推荐</tui-divider>
@@ -227,6 +145,9 @@ export default {
 		console.log(this.user.userinfo)
 	},
 	methods: {
+    scanCode(e){
+      this.helper.navBtns.handle({'uni_code':'scan'})
+    },
 		href(page) {
 			let url = '';
 			switch (page) {
@@ -344,8 +265,8 @@ export default {
 	position: relative;
 }
 
-.tui-icon-setup {
-	margin-left: 8rpx;
+.tui-icon-tools {
+	margin-left: 18rpx;
 }
 
 .tui-badge {
@@ -375,16 +296,6 @@ export default {
 	color: #f74d54;
 }
 
-.tui-badge-dot {
-	position: absolute;
-	height: 12rpx;
-	width: 12rpx;
-	border-radius: 50%;
-	right: -12rpx;
-	top: 0;
-	background: #f74d54;
-}
-
 .tui-mybg-box {
 	width: 100%;
 	height: 464rpx;
@@ -402,7 +313,7 @@ export default {
 	width: 100%;
 	height: 128rpx;
 	left: 0;
-	top: 120rpx;
+	top: 150rpx;
 	padding: 0 30rpx;
 	box-sizing: border-box;
 	display: flex;
@@ -459,7 +370,7 @@ export default {
 	box-sizing: border-box;
 	position: absolute;
 	left: 0;
-	top: 280rpx;
+	top: 340rpx;
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
@@ -487,140 +398,43 @@ export default {
 }
 
 .tui-content-box {
-	width: 100%;
+	/* width: 100%;
 	padding: 0 30rpx;
 	box-sizing: border-box;
 	position: relative;
 	top: -72rpx;
-	z-index: 10;
+	z-index: 10; */
 }
 
-.tui-box {
-	width: 100%;
-	background: #fff;
-	box-shadow: 0 3rpx 20rpx rgba(183, 183, 183, 0.1);
-	border-radius: 10rpx;
-	overflow: hidden;
-}
-
-.tui-order-box {
-	height: 208rpx;
-}
-
-.tui-cell-header {
-	width: 100%;
-	height: 74rpx;
-	padding: 0 26rpx;
-	box-sizing: border-box;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-}
-
-.tui-cell-title {
-	font-size: 30rpx;
-	line-height: 30rpx;
-	font-weight: 600;
-	color: #333;
-}
-
-.tui-cell-sub {
-	font-size: 26rpx;
-	font-weight: 400;
-	color: #999;
-	padding-right: 28rpx;
-}
-
-.tui-order-list {
-	width: 100%;
-	height: 134rpx;
-	padding: 0 30rpx;
-	box-sizing: border-box;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-}
-
-.tui-order-item {
-	flex: 1;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-}
-
-.tui-order-text,
-.tui-tool-text {
-	font-size: 26rpx;
-	font-weight: 400;
-	color: #666;
-	padding-top: 4rpx;
-}
-
-.tui-tool-text {
-	font-size: 24rpx;
-}
-
-.tui-order-icon {
-	width: 56rpx;
-	height: 56rpx;
-	display: block;
-}
-
-.tui-assets-box {
-	height: 178rpx;
-	margin-top: 20rpx;
-}
-
-.tui-assets-list {
-	height: 84rpx;
-}
-
-.tui-assets-num {
-	font-size: 32rpx;
-	font-weight: 500;
-	color: #333;
-	position: relative;
-}
-
-.tui-assets-text {
-	font-size: 24rpx;
-	font-weight: 400;
-	color: #666;
-	padding-top: 6rpx;
-}
-
-.tui-tool-box {
-	margin-top: 20rpx;
-}
-
-.tui-flex-wrap {
-	flex-wrap: wrap;
-	height: auto;
-	padding-bottom: 30rpx;
-}
-
-.tui-tool-item {
-	width: 25%;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	flex-direction: column;
-	padding-top: 30rpx;
-}
-
-.tui-tool-icon {
-	width: 64rpx;
-	height: 64rpx;
-	display: block;
-}
-
-.tui-badge-icon {
-	width: 66rpx;
-	height: 30rpx;
-	position: absolute;
-	right: 0;
-	transform: translateX(88%);
-	top: -15rpx;
-}
+/* ====================== */
+	.tui-item-box {
+		width: 100%;
+		display: flex;
+		align-items: center;
+	}
+  .tui-list-cell_name {
+  	padding-left: 20rpx;
+  	display: flex;
+  	align-items: center;
+  	justify-content: center;
+  }
+  
+  .tui-ml-auto {
+  	margin-left: auto;
+  }
+  
+  .tui-right {
+  	margin-left: auto;
+  	margin-right: 34rpx;
+  	font-size: 26rpx;
+  	color: #999;
+  }
+  
+  .tui-logo {
+  	height: 52rpx;
+  	width: 52rpx;
+  	flex-shrink: 0;
+  }
+  
 
 </style>
