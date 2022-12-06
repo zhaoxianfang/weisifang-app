a029a2ec4105a254c4c65ed005e01d93a3ae8329
zhaoxianfang <1748331509@qq.com>
2022-12-06 09:15:34 +0800

    fix

---
+++ js_sdk/weisifang/baIdCode.js
@@ -0,0 +1,49 @@
+// 获取设备唯一标识（OAID、AAID、IMEI等）
+// https://ext.dcloud.net.cn/plugin?id=10318
+
+// #ifdef APP-PLUS
+const idCode = uni.requireNativePlugin('Ba-IdCode')
+
+import helper from './helper.js'
+// #endif
+
+const baIdCode = {
+    register() { //注册，先注册再获取，注意APP合规性，若最终用户未同意隐私政策则不要调用
+        idCode.register(
+            res => {
+                console.log(res);
+                uni.showToast({
+                    title: res.msg,
+                    icon: "none",
+                    duration: 3000
+                })
+            });
+    },
+    getIdCodes() { //获取设备的各种标识码
+        idCode.getIdCodes(res => {
+            console.log(res);
+            if (res.data) {
+                this.msgList.unshift(JSON.stringify(res.data))
+            }
+            uni.showToast({
+                title: res.msg,
+                icon: "none",
+                duration: 3000
+            })
+        });
+    },
+    getOAID() { //异步获取 OAID
+        idCode.getOAID(res => {
+            console.log(res);
+            if (res.data) {
+                this.msgList.unshift(JSON.stringify(res.data))
+            }
+            uni.showToast({
+                title: res.msg,
+                icon: "none",
+                duration: 3000
+            })
+        });
+    },
+}
+export default baIdCode
---
+++ js_sdk/weisifang/baLocation.js
@@ -0,0 +1,120 @@
+// 实时定位（系统、后台运行、支持息屏）
+// https://ext.dcloud.net.cn/plugin?id=10330
+
+// #ifdef APP-PLUS
+const location = uni.requireNativePlugin('Ba-Location')
+const minTimeMs = 30
+const minDistanceM = 1
+import helper from './helper.js'
+// #endif
+
+const baLocation = {
+    startL() { //开启定位
+        location.start(
+            res => {
+                console.log(res);
+                uni.showToast({
+                    title: res.msg,
+                    icon: "none",
+                    duration: 3000
+                })
+            }, {
+                minTimeMs: minTimeMs,
+                minDistanceM: minDistanceM
+            });
+    },
+    stopL() { //关闭定位
+        location.stop(res => {
+            console.log(res);
+            uni.showToast({
+                title: res.msg,
+                icon: "none",
+                duration: 3000
+            })
+        });
+    },
+    isLocationService() { //定位服务是否开启
+        location.isLocationService(
+            res => {
+                console.log(res);
+                if (res.data) {
+                    this.msgList.unshift(JSON.stringify(res.data))
+                    this.msgList.unshift(dateUtil.now())
+                }
+                uni.showToast({
+                    title: res.msg,
+                    icon: "none",
+                    duration: 3000
+                })
+            });
+    },
+    isLocationEnable() { //定位开关是否打开
+        location.isLocationEnable(
+            res => {
+                console.log(res);
+                if (res.data) {
+                    this.msgList.unshift(JSON.stringify(res.data))
+                    this.msgList.unshift(dateUtil.now())
+                }
+                uni.showToast({
+                    title: res.msg,
+                    icon: "none",
+                    duration: 3000
+                })
+            });
+    },
+    goSetting() { //跳转到定位服务设置界面
+        location.goSetting(
+            res => {
+                console.log(res);
+                uni.showToast({
+                    title: res.msg,
+                    icon: "none",
+                    duration: 3000
+                })
+            });
+    },
+    subLocation() { //订阅定位信息
+        location.subLocation(res => {
+            console.log(res);
+            if (res.data) {
+                this.msgList.unshift(JSON.stringify(res.data))
+                this.msgList.unshift(dateUtil.now())
+            }
+            uni.showToast({
+                title: res.msg,
+                icon: "none",
+                duration: 3000
+            })
+        });
+    },
+    subLocationStatus() { //订阅定位状态变化
+        location.subLocationStatus(res => {
+            console.log(res);
+            if (res.data) {
+                this.msgList.unshift(JSON.stringify(res.data))
+                this.msgList.unshift(dateUtil.now())
+            }
+            uni.showToast({
+                title: res.msg,
+                icon: "none",
+                duration: 3000
+            })
+        });
+    },
+    subGpsStatus() { //订阅 gps和 搜星变化
+        location.subGpsStatus(res => {
+            console.log(res);
+            if (res.data) {
+                this.msgList.unshift(JSON.stringify(res.data))
+                this.msgList.unshift(dateUtil.now())
+            }
+            uni.showToast({
+                title: res.msg,
+                icon: "none",
+                duration: 3000
+            })
+        });
+    },
+}
+export default baLocation
